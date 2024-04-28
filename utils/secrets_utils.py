2a62dd99b33c4d4dcbce759d6c30be199e798dca
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2024-04-28 09:24:12 +0100

    Test for invalid checksum

--- utils/secrets_utils.py
+++ utils/secrets_utils.py
@@ -1,3 +1,4 @@
+import google_crc32c
 from decouple import config
 from google.cloud import secretmanager
 
@@ -10,13 +11,19 @@ class GoogleCloudsSecretManager:
     def __init__(self):
         self.client = secretmanager.SecretManagerServiceClient()
 
-    def access_secret(self, secret_id, version_id="latest"):
-
-        # TODO: Payloadcheck? as in the docs...
-
+    def access_secret(self, secret_id: str, version_id="latest") -> str:
         environment = config("ENV")
+        google_cloud_project_name = config("GC_PROJECT_NAME")
         name = (
-            f"projects/blogity/secrets/{secret_id}-{environment}/versions/{version_id}"
+            f"projects/{google_cloud_project_name}/secrets/{secret_id}-"
+            f"{environment}/versions/{version_id}"
         )
         response = self.client.access_secret_version(request={"name": name})
+
+        crc32c = google_crc32c.Checksum()
+        crc32c.update(response.payload.data)
+        if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
+            # TODO: Log the incident
+            return "Data corruption detected."
+
         return response.payload.data.decode("utf-8")
--- utils/tests.py
+++ utils/tests.py
@@ -1,29 +1,40 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
+import google_crc32c
+
 from utils.secrets_utils import GoogleCloudsSecretManager
 
 
 class SecretAccess(unittest.TestCase):
-
     # Expecting that the app is authenticated to Google clouds
 
-    def setUp(self):
-        self.env = "DEV"
+    mock = MagicMock()
+    checksum_mock = MagicMock()
+    checksum = google_crc32c.Checksum()
+    checksum_mock.return_value = google_crc32c.Checksum()
+    mock_client = mock.return_value.access_secret_version.return_value = MagicMock(
+        payload=MagicMock(data=b"MySecret")
+    )
 
-    @patch("utils.secrets_utils.secretmanager.SecretManagerServiceClient")
-    @patch("utils.secrets_utils.config")
-    def test_successful_retrieval(self, mock_config, mock_client_class):
+    @patch("utils.secrets_utils.secretmanager.SecretManagerServiceClient", new=mock)
+    @patch("utils.secrets_utils.google_crc32c.Checksum", new=checksum_mock)
+    def test_successful_retrieval(self):
+        self.checksum.update(b"MySecret")
+        self.mock_client.payload.data_crc32c = int(self.checksum.hexdigest(), 16)
 
-        mock_config.return_value = "DEV"
-
-        mock_client = mock_client_class.return_value
-        mock_client.access_secret_version.return_value = MagicMock(
-            payload=MagicMock(data=b"MySecret")
+        result = GoogleCloudsSecretManager().access_secret(
+            self.mock_client, "my_secret"
         )
+        self.assertEqual("MySecret", result)
 
-        result = GoogleCloudsSecretManager().access_secret(mock_client, "my_secret")
-        self.assertEqual(result, "MySecret")
+    @patch("utils.secrets_utils.secretmanager.SecretManagerServiceClient", new=mock)
+    def test_invalid_checksum(self):
+        self.mock_client.payload.data_crc32c = 0
+        result = GoogleCloudsSecretManager().access_secret(
+            self.mock_client, "my_secret"
+        )
+        self.assertEqual("Data corruption detected.", result)
 
 
 if __name__ == "__main__":
