8e6b2d8b54f100fc5107cee30976e0f8d2671f12
afk0901 <arnarfreyr16@gmail.com>
2024-08-04 23:46:26 +0100

    Reverting selfsigned certs changes and configuration changes

--- docker-runserver.sh
+++ docker-runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python manage.py collectstatic --noinput --settings Bloggity.settings.production && \
 python manage.py migrate --settings Bloggity.settings.production && \
-exec gunicorn Bloggity.wsgi:application --bind 0.0.0.0:443
+exec gunicorn Bloggity.wsgi:application --bind 0.0.0.0:8080
 # Uncomment this if testing locally with self signed certificates.
 --certfile cert.pem --keyfile key.pem
