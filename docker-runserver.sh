472c427b0133d91ad5dc8f7f215540787c080b11
Arnar Freyr Kristinsson <arnar11@hotmail.com>
2024-08-04 23:58:22 +0100

    Update docker-runserver.sh

--- docker-runserver.sh
+++ docker-runserver.sh
@@ -3,4 +3,4 @@ python manage.py collectstatic --noinput --settings Bloggity.settings.production
 python manage.py migrate --settings Bloggity.settings.production && \
 exec gunicorn Bloggity.wsgi:application --bind 0.0.0.0:443
 # Uncomment this if testing locally with self signed certificates.
-#--certfile cert.pem --keyfile key.pem
+--certfile cert.pem --keyfile key.pem
