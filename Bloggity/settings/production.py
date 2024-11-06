579359700c76aecb4bc9107e0183a46dc2dc7bd5
Arnar Freyr Kristinsson <arnarfkr@gmail.com>
2024-11-06 18:15:47 +0000

    Adding the internal URL to ALLOWED hosts (#115)

--- Bloggity/settings/production.py
+++ Bloggity/settings/production.py
@@ -58,6 +58,7 @@ ALLOWED_HOSTS = [
     "arnarfreyr.is",
     "localhost",
     "nginx-463509866170.europe-north1.run.app",
+    "blogity-staging-463509866170.europe-north1.run.app",
 ]
 
 CORS_ALLOWED_ORIGINS = [
